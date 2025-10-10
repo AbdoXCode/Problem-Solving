d6b27c7deccd6e783c8adb5c6b8731945361f861
Abdelrhman <adodoadodo2025@gmail.com>
2025-10-10 19:00:24 +0300

    solution of Y. Range sum query

--- my.cpp
+++ my.cpp
@@ -12,22 +12,33 @@
 using namespace std;
 
 int main() {
+    int n,q;
 
-    string s;
-    cin>>s;
+    cin>>n>>q;
 
-    int freq[26] = {0};
+    long long arr[100005];
 
-    bool isValid = true;
-
-    for (int i =0;i<s.size();i++) {
-        freq[s[i] - 'a']++;
+    for (int i =0;i<n;i++) {
+        cin>>arr[i];
+    }
+    for (int i = 1; i<n;i++) {
+        arr[i] = arr[i] + arr[i-1];
     }
 
-    for (int i =0 ;i< 26;i++) {
-        if (freq[i] == 0) {
-            isValid = false;
+
+    while (q--) {
+        int l,r;
+        cin>>l>>r;
+
+        l--;r--;
+
+        if (l ==0) {
+            cout<<arr[r]<<endl;
+        }
+        else {
+            cout<<arr[r] - arr[l - 1]<<endl;
         }
     }
-    cout<< (isValid ? "valid":"not valid");
+
+
 }
