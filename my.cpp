144c66f9b219d9e29661a3e2fd16f169d0723686
Abdelrhman <adodoadodo2025@gmail.com>
2025-10-10 10:52:17 +0300

    solving 2D array problem

--- my.cpp
+++ my.cpp
@@ -11,9 +11,30 @@
 
 using namespace std;
 
-
-
 int main() {
 
+    int arr[3][3];
 
+    for (int r=0;r<3;r++) {
+        for (int c=0;c<3;c++) {
+            cin>>arr[r][c];
+        }
+    }
+
+    for (int r=0;r<3;r++) {
+        int sumofEven =0,sumofOdd = 0;
+
+        for (int c=0;c<3;c++) {
+            if (arr[r][c] % 2 == 0) {
+                sumofEven+= arr[r][c];
+            }else {
+                sumofOdd += arr[r][c];
+            }
+        }
+        cout<<sumofEven<<" " <<sumofOdd;
+        cout<<endl;
+
+    }
+
+    return 0;
 }
