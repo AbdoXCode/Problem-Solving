abf36e3e597ded737b4ffe8932079e470af3b1c6
Abdelrhman <adodoadodo2025@gmail.com>
2025-10-12 20:56:31 +0300

    A. Watermelon

--- my.cpp
+++ my.cpp
@@ -6,21 +6,10 @@ int main() {
     int x;
     cin>>x;
 
-    int counter = 0;
-
-    while (x>0) {
-        if (x >= 100) {
-            x-=100;
-        }else if (x >= 20) {
-            x-=20;
-        }else if (x >= 5) {
-            x-=5;
-        }else if (x >= 1) {
-            x-=1;
-        }
-        counter++;
-
+    if (x >2 && x % 2 == 0) {
+        cout<<"YES";
+    }else {
+        cout<<"NO";
     }
 
-    cout<<counter;
 }
