250ca76962f8dd40aa778c67368a90894ce5ab8f
Abdelrhman <adodoadodo2025@gmail.com>
2025-10-12 19:32:57 +0300

    A. Domino piling

--- my.cpp
+++ my.cpp
@@ -1,29 +1,10 @@
-#include <algorithm>
 #include <iostream>
-#include <vector>
-#include<stack>
 
 using namespace std;
 
 int main() {
-    stack<char>s;
+    int m,n;
+    cin>>m>>n;
 
-    string str;
-    cin>>str;
-
-    for (int i =0;i<str.size();i++) {
-        if (str[i] == '{') {
-            s.push('{');
-        }else if (!s.empty() && str[i] == '}' && s.top() == '{'){
-            s.pop();
-        }else {
-            s.push('}');
-        }
-    }
-
-    if (s.empty()) {
-        cout<<"Regular";
-    }else {
-        cout<<"Not Regular";
-    }
+    cout<<(m * n) / 2;
 }
