6dc7e52abb06e083896d9d62cd3ef6f19bffc1aa
Abdelrhman <adodoadodo2025@gmail.com>
2025-10-12 16:02:41 +0300

    Applicaton on Stack

--- my.cpp
+++ my.cpp
@@ -1,33 +1,29 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
-
-
+#include<stack>
 
 using namespace std;
 
 int main() {
-    int n;
-    cin>>n;
-    vector<int> v;
+    stack<char>s;
 
-    for (int i =0;i<n;i++) {
-        int x;
-        cin>>x;
+    string str;
+    cin>>str;
 
-        v.push_back(x);
-    }
-
-    sort(v.begin(),v.end());
-
-    int c = 1;
-    for (int i =0;i<v.size();i++) {
-        if (v[i] != c) {
-            break;
+    for (int i =0;i<str.size();i++) {
+        if (str[i] == '{') {
+            s.push('{');
+        }else if (!s.empty() && str[i] == '}' && s.top() == '{'){
+            s.pop();
+        }else {
+            s.push('}');
         }
-        c++;
     }
 
-    cout<<c<<endl;
-
+    if (s.empty()) {
+        cout<<"Regular";
+    }else {
+        cout<<"Not Regular";
+    }
 }
