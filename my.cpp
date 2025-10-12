2d9e93670ab09a6774d860fbde401f4a124d60bd
Abdelrhman <adodoadodo2025@gmail.com>
2025-10-12 11:09:15 +0300

    Implementing Vector Exercise

--- my.cpp
+++ my.cpp
@@ -1,44 +1,33 @@
 #include <algorithm>
-#include <bitset>
 #include <iostream>
-#include <cmath>
-#include <map>
 #include <vector>
-#include <unordered_map>
-#include <stack>
 
 
 
 using namespace std;
 
 int main() {
-    int n,q;
-
-    cin>>n>>q;
-
-    long long arr[100005];
+    int n;
+    cin>>n;
+    vector<int> v;
 
     for (int i =0;i<n;i++) {
-        cin>>arr[i];
+        int x;
+        cin>>x;
+
+        v.push_back(x);
     }
-    for (int i = 1; i<n;i++) {
-        arr[i] = arr[i] + arr[i-1];
-    }
 
+    sort(v.begin(),v.end());
 
-    while (q--) {
-        int l,r;
-        cin>>l>>r;
-
-        l--;r--;
-
-        if (l ==0) {
-            cout<<arr[r]<<endl;
+    int c = 1;
+    for (int i =0;i<v.size();i++) {
+        if (v[i] != c) {
+            break;
         }
-        else {
-            cout<<arr[r] - arr[l - 1]<<endl;
-        }
+        c++;
     }
 
+    cout<<c<<endl;
 
 }
