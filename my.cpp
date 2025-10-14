680d830d9a83b942cbbc9ee2b22e5a4b3d28d40f
Abdelrhman <adodoadodo2025@gmail.com>
2025-10-14 18:14:11 +0300

    B. Taxi

--- my.cpp
+++ my.cpp
@@ -6,30 +6,54 @@
 using namespace std;
 
 int main() {
-    int t;
-    cin>>t;
+    int n;
+    cin>>n;
 
+    int arr[1000];
+    int freq[1000];
+    int taxiNum = 0;
 
-
-    while (t--) {
-        vector<long long> answers;
-        long long n;
-        cin>>n;
-
-        for (int i =1;i<18;i++) {
-            long long val = 1 + (long long)pow(10,i);
-
-            if (n % val == 0) {
-                answers.push_back(n / val);
-            }
-        }
-
-        cout<<answers.size()<<endl;
-
-        reverse(answers.begin(),answers.end());
-        for (int i =0 ; i< answers.size();i++) {
-            cout<<answers[i]<<" ";
-        }
-        cout<<endl;
+    for (int i = 0;i<n;i++) {
+        cin>>arr[i];
+        freq[arr[i]]++;
     }
+
+    taxiNum += freq[4];
+
+   while (freq[1] != 0 && freq[3] != 0) {
+       freq[3]--;
+       freq[1]--;
+       taxiNum++;
+   }
+
+    if (freq[1] == 0 && freq[3] !=0) {
+        taxiNum+=freq[3];
+        freq[3] = 0;
+    }
+
+    taxiNum +=freq[2]/2;
+
+
+    if (freq[2] %2 !=0) {
+        if (freq[1] <=2) {
+            freq[2] = 0;
+            freq[1] = 0;
+        }else {
+            freq[1] -= 2;
+            freq[2] = 0;
+        }
+        taxiNum++;
+    }
+
+
+    if (freq[1] !=0) {
+        taxiNum = taxiNum + freq[1] / 4;
+
+        if (freq[1] % 4 != 0) {
+            taxiNum++;
+        }
+
+    }
+
+    cout<<taxiNum;
 }
