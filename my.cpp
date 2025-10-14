ad5b5d4da4787b813df945fa073b334c00b59a5f
Abdelrhman <adodoadodo2025@gmail.com>
2025-10-14 14:53:15 +0300

    2132B - The Secret Number

--- my.cpp
+++ my.cpp
@@ -1,22 +1,35 @@
+#include <algorithm>
 #include <iostream>
 #include<cmath>
+#include <vector>
 
 using namespace std;
 
 int main() {
-    int cubes;
-    cin>>cubes;
+    int t;
+    cin>>t;
 
-    int height = 0;
 
-    int required_cubes = 0;
 
-    while (required_cubes <= cubes) {
-        height++;
-        cubes-=required_cubes;
-        required_cubes = (height*(height +1)) / 2;
+    while (t--) {
+        vector<long long> answers;
+        long long n;
+        cin>>n;
 
+        for (int i =1;i<18;i++) {
+            long long val = 1 + (long long)pow(10,i);
+
+            if (n % val == 0) {
+                answers.push_back(n / val);
+            }
+        }
+
+        cout<<answers.size()<<endl;
+
+        reverse(answers.begin(),answers.end());
+        for (int i =0 ; i< answers.size();i++) {
+            cout<<answers[i]<<" ";
+        }
+        cout<<endl;
     }
-    cout<<height-1;
-
 }
