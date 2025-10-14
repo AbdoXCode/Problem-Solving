93b014af1d995f832bd7df9279568a3e56766da0
Abdelrhman <adodoadodo2025@gmail.com>
2025-10-14 20:36:20 +0300

    Implementation of Set

--- my.cpp
+++ my.cpp
@@ -1,59 +1,28 @@
 #include <algorithm>
 #include <iostream>
-#include<cmath>
-#include <vector>
+#include<set>
 
 using namespace std;
 
 int main() {
+    set<int>s;
     int n;
     cin>>n;
 
-    int arr[1000];
-    int freq[1000];
-    int taxiNum = 0;
+    for (int i =0;i<n;i++) {
+        int x;
+        cin>>x;
 
-    for (int i = 0;i<n;i++) {
-        cin>>arr[i];
-        freq[arr[i]]++;
+        s.insert(x);
     }
 
-    taxiNum += freq[4];
+    cout<<s.size()<<endl;
 
-   while (freq[1] != 0 && freq[3] != 0) {
-       freq[3]--;
-       freq[1]--;
-       taxiNum++;
-   }
+    auto it = s.begin();
 
-    if (freq[1] == 0 && freq[3] !=0) {
-        taxiNum+=freq[3];
-        freq[3] = 0;
+    while (it != s.end()) {
+        cout<<*it<<" ";
+        it++;
     }
 
-    taxiNum +=freq[2]/2;
-
-
-    if (freq[2] %2 !=0) {
-        if (freq[1] <=2) {
-            freq[2] = 0;
-            freq[1] = 0;
-        }else {
-            freq[1] -= 2;
-            freq[2] = 0;
-        }
-        taxiNum++;
-    }
-
-
-    if (freq[1] !=0) {
-        taxiNum = taxiNum + freq[1] / 4;
-
-        if (freq[1] % 4 != 0) {
-            taxiNum++;
-        }
-
-    }
-
-    cout<<taxiNum;
 }
