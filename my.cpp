7bd4ce9c7b3f21acde34bc86ed2da86d6aa770bf
Abdelrhman <adodoadodo2025@gmail.com>
2025-10-15 14:03:50 +0300

    A. Magnets

--- my.cpp
+++ my.cpp
@@ -3,30 +3,26 @@
 using namespace std;
 
 int main() {
-    int n;
-    cin>>n;
+    int t;
+    cin>>t;
 
-    int arr[10000];
+    int group = 1;
 
-    for (int i = 0;i <n-1;i++) {
-        int x;
+    int n = t;
+    string arr[100005];
+
+    while (t--) {
+        string x;
         cin>>x;
-        arr[i] = x;
+
+        arr[t] = x;
     }
 
-    for (int i = 1; i<n - 1;i++) {
-        arr[i] = arr[i] + arr[i-1];
+    for (int i = 1; i<n;i++) {
+        if (arr[i] == arr [i-1]) {
+        }else {
+            group +=1;
+        }
     }
-
-    int r1,r2;
-    cin>>r1>>r2;
-
-    r1--;
-    r2-=2;
-
-    if (r1 == 0) {
-        cout<<arr[r2];
-    }else {
-        cout<<arr[r2] - arr[r1-1];
-    }
+    cout<<group;
 }
