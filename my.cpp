08b076c41dea9654718e9fd9b62859f897d78a29
Abdelrhman <adodoadodo2025@gmail.com>
2025-10-16 23:12:35 +0300

    M. Replace MinMax

--- my.cpp
+++ my.cpp
@@ -7,14 +7,33 @@ int main() {
     cin>>x;
 
     int arr[x];
+    int maxi = 0,mini =1000000;
 
+    int maxiIndex,miniIndex;
     for (int i =0;i<x;i++) {
         cin>>arr[i];
     }
 
+
     for (int i =0;i<x;i++) {
-        if (arr[i] <= 10) {
-            cout<<"A["<<i<<"]"<<" = "<<arr[i]<<endl;
+        maxi = max(arr[i],maxi);
+        mini = min(arr[i] , mini);
+    }
+
+    for (int i =0;i<x;i++) {
+
+        if (arr[i] == maxi) {
+            maxiIndex =i;
         }
+        if (arr[i] == mini) {
+            miniIndex = i;
+        }
+    }
+    swap(arr[maxiIndex],arr[miniIndex]);
+
+
+
+    for (int i =0;i<x;i++) {
+        cout<<arr[i]<<" ";
     }
 }
