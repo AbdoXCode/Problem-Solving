8cfe2e537c72ce8ac7823f718f5621a00690c783
Abdelrhman <adodoadodo2025@gmail.com>
2025-10-18 22:17:43 +0300

    A. Again Twenty Five!

--- my.cpp
+++ my.cpp
@@ -5,45 +5,8 @@
 using namespace std;
 
 int main() {
-    int x,y;
-    cin>>x>>y;
+    double n;
+    cin>>n;
 
-    int arr[100005];
-
-    for (int i =0 ;i <x;i++) {
-        cin>>arr[i];
-    }
-
-    sort(arr,arr+x);
-
-    while (y--) {
-        int target;
-        cin>>target;
-
-        int left = 0;
-        int right = x-1;
-
-        bool flag = false;
-
-        while (left <= right) {
-            int middle = (left + right) /2;
-
-
-            if (arr[middle] == target) {
-                flag = true;
-                break;
-            }else if (target < arr[middle]) {
-                right = middle - 1;
-            }else if (target > arr[middle]) {
-                left = middle + 1;
-            }
-        }
-
-        if (flag) {
-            cout<<"found"<<endl;
-        }else {
-            cout<<"not found"<<endl;
-        }
-
-    }
+    cout<<25;
 }
