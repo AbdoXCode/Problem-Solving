2f4e43d5cf3a7f1b4ec3b71352e59b61dbf60206
Abdelrhman <adodoadodo2025@gmail.com>
2025-10-25 22:00:02 +0300

    H. Diverse Team

--- my.cpp
+++ my.cpp
@@ -1,32 +1,37 @@
+#include <algorithm>
 #include <iostream>
 #include <map>
 #include <queue>
+#include <set>
 #include <stack>
 
 using namespace std;
 
 int main() {
+    int n,teams;
+    cin>>n>>teams;
 
-    queue<int>q;
+    set<int>Nset;
+    vector<int> index;
 
-    int t;
-    cin>>t;
-
-    while (t--) {
-        int x,v;
-        cin>>x>>v;
-
-        if (x == 1) {
-            q.push(v);
-        }else {
-            if (q.front() == v) {
-                cout<<"Yes"<<endl;
-            }else {
-                cout<<"No"<<endl;
-            }
-            q.pop();
+    for (int i = 0;i <n;i++) {
+        int x;
+        cin>>x;
+        if (Nset.count(x) == 0) {
+            Nset.insert(x);
+            index.push_back(i);
         }
-
     }
+
+    if (Nset.size() >= teams) {
+        cout<<"YES"<<endl;
+        for (int i =0;i<teams;i++) {
+            cout<<index[i] + 1<<" ";
+        }
+    }else {
+        cout<<"NO"<<endl;
+    }
+
+
 
 }
