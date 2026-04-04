261e6dbf53ffa675af5d048f983485009211348b
Abdelrhman <adodoadodo2025@gmail.com>
2026-04-05 00:17:42 +0200

    A. String Task

--- my.cpp
+++ my.cpp
@@ -8,56 +8,33 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);cin.tie(nullptr);
 
-    map<char,int> letters = {
-        {'a',1},
-        {'b',2},
-        {'c',3},
-        {'d',4},
-        {'e',5},
-        {'f',6},
-        {'g',7},
-        {'h',8},
-        {'i',9},
-        {'j',10},
-        {'k',11},
-        {'l',12},
-        {'m',13},
-        {'n',14},
-        {'o',15},
-        {'p',16},
-        {'q',17},
-        {'r',18},
-        {'s',19},
-        {'t',20},
-        {'u',21},
-        {'v',22},
-        {'w',23},
-        {'x',24},
-        {'y',25},
-        {'z',26}
-    };
+    string x;
+    cin>>x;
 
-    string x,y;
-    cin>>x>>y;
+    string y;
 
-    int length = (x.length() < y.length()) ? x.length() : y.length();
+    char vowels[6] = { 'A', 'O', 'Y', 'E', 'U', 'I'};
 
-    bool isfound = false;
-    for (int i = 0; i < length; i++) {
-        if (letters[x[i]] == letters[y[i]]) {
-            continue;
-        }else if (letters[x[i]] >= letters[y[i]]) {
-            cout<<y<<endl;
-            isfound = true;
-            break;
-        }else {
-            cout<<x<<endl;
-            isfound = true;
-            break;
+
+    for (int i =0;i<x.length();i++) {
+
+        char c = tolower(x[i]);
+
+        bool isVowel = false;
+
+        for (int j =0; j< 6;j++) {
+            if (c == std::tolower(vowels[j])) {
+                isVowel = true;
+                break;
+            }
+        }
+
+        if (!isVowel) {
+            y+=".";
+            y+=tolower(x[i]);
         }
     }
 
-    if (!isfound) {
-        cout << ((x.length() > y.length()) ? y : x);
-    }
+    cout<<y;
+
 }
