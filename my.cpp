84dcd23c35d3341df98706f3fcffd436d42ce723
Abdelrhman <adodoadodo2025@gmail.com>
2026-04-15 20:00:24 +0200

    H. Simple Mod Assiut university sheet

--- my.cpp
+++ my.cpp
@@ -1,16 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long divideBy2(long long n) {
-    int count = 0;
-
-    while (n%2 == 0 && n!=0) {
-        n = n/2;
-        count++;
-    }
-
-    return count;
-}
 
 int main (){
     ios_base::sync_with_stdio(false); cin.tie(nullptr);
@@ -18,14 +8,5 @@ int main (){
     int n;
     cin>>n;
 
-    long long x;
-    long long maximum = 0;
-
-    for (int i =0 ;i<n;i++) {
-        cin>>x;
-
-        maximum = max(maximum,divideBy2(x));
-    }
-
-    cout<<maximum;
+    cout<<"0 0"<<endl;
 }
