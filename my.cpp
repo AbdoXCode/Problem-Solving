b7ebb4b47249920669868fea2f5fc96e10f45ffa
Abdelrhman <adodoadodo2025@gmail.com>
2026-04-17 08:32:36 +0200

    G. Conversion Assiut university sheet

--- my.cpp
+++ my.cpp
@@ -5,18 +5,18 @@ using namespace std;
 int main (){
     ios_base::sync_with_stdio(false); cin.tie(nullptr);
 
-    int n;
-    cin>>n;
+    string s;
+    cin>>s;
 
-    while (n--) {
-        string x;
-        cin>>x;
-
-        if (x.length() > 10) {
-            cout<<x[0] <<x.length() -2 <<x[x.length() -1]<<endl;
-        }else {
-            cout<<x<<endl;
+    for (int i =0; i<s.length();i++) {
+        if (s[i] == ',') {
+            s[i] = ' ';
+        }else if (s[i] >= 'A' & s[i] <='Z') {
+            s[i] = s[i] + 32;
+        }else if (s[i] >= 'a' & s[i] <='z') {
+            s[i] = s[i] - 32;
         }
     }
+    cout<<s;
 
 }
