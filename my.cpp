1df555a2aa0131a5c7d7c232c4ef8aa19e07db7a
Abdelrhman <adodoadodo2025@gmail.com>
2026-04-18 18:32:58 +0200

    B. Let's use Getline Assiut university sheet

--- my.cpp
+++ my.cpp
@@ -5,18 +5,16 @@ using namespace std;
 int main (){
     ios_base::sync_with_stdio(false); cin.tie(nullptr);
 
-    string s;
-    cin>>s;
+    string s,final;
+    getline(cin,s);
 
-    for (int i =0; i<s.length();i++) {
-        if (s[i] == ',') {
-            s[i] = ' ';
-        }else if (s[i] >= 'A' & s[i] <='Z') {
-            s[i] = s[i] + 32;
-        }else if (s[i] >= 'a' & s[i] <='z') {
-            s[i] = s[i] - 32;
+    for (char c: s) {
+        if (c == '\\') {
+            break;
         }
+
+        final +=c;
     }
-    cout<<s;
+    cout<<final;
 
 }
