894bbb14bece66b4356262e6c9b8bb48882b8d6b
Abdelrhman <adodoadodo2025@gmail.com>
2026-04-28 14:05:25 +0300

    C. Wonderful Number university sheet

--- my.cpp
+++ my.cpp
@@ -1,20 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-auto print(int x) {
-    for (int i = 1 ;i<=x;i++) {
-        if (x==i) {
-            cout<<i;
+string palindrome(string cc) {
+    string palindrome;
+
+    for (long long i =cc.size() -1 ;i>=0;i--) {
+        palindrome+=(cc[i]);
+    }
+    return palindrome;
+}
+string wonderfull(long long x) {
+    if (x % 2 != 0) {
+        string binary = bitset<32>(x).to_string();
+
+        binary = binary.substr(binary.find('1'));
+
+        string xx = palindrome(binary);
+        if (xx == binary) {
+            return "YES";
         }else {
-            cout<<i<<" ";
+            return "NO";
         }
+    }else {
+        return "NO";
     }
 }
 
 int main () {
     ios_base::sync_with_stdio(false); cin.tie(nullptr);
-    int x;
+    long long x;
     cin>>x;
 
-    print(x);
+    cout<<wonderfull(x);
 }
