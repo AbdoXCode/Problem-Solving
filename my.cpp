4652998b3509fe9934b4cfeedf8ff0928b6c2a1b
Abdelrhman <adodoadodo2025@gmail.com>
2026-04-28 14:32:22 +0300

    D. Prime Function university sheet

--- my.cpp
+++ my.cpp
@@ -1,35 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string palindrome(string cc) {
-    string palindrome;
-
-    for (long long i =cc.size() -1 ;i>=0;i--) {
-        palindrome+=(cc[i]);
-    }
-    return palindrome;
-}
-string wonderfull(long long x) {
-    if (x % 2 != 0) {
-        string binary = bitset<32>(x).to_string();
-
-        binary = binary.substr(binary.find('1'));
-
-        string xx = palindrome(binary);
-        if (xx == binary) {
-            return "YES";
-        }else {
-            return "NO";
-        }
-    }else {
+string prime(long long x) {
+    if (x <= 1) {
         return "NO";
     }
+
+    for (long long i = 2; i * i<=x;i++) {
+        if (x % i == 0) {
+            return "NO";
+        }
+    }
+    return "YES";
 }
 
-int main () {
-    ios_base::sync_with_stdio(false); cin.tie(nullptr);
-    long long x;
-    cin>>x;
+int main() {
+    int n;
+    cin>>n;
 
-    cout<<wonderfull(x);
+    while (n--) {
+        long long x;
+        cin>>x;
+
+        cout<<prime(x)<<endl;
+    }
 }
