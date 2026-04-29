5ab0ed36273c76d655cdcf6de4dab9463d10e989
Abdelrhman <adodoadodo2025@gmail.com>
2026-04-29 15:00:06 +0300

    H. N Times Assiut university sheet

--- my.cpp
+++ my.cpp
@@ -1,33 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int findMinMax(int arr[] ,int size) {
-
-    long long min = 10000000;
-    long long max = -10000000;
-
-    for (int i =0;i<size;i++) {
-        if (arr[i] < min) {
-            min = arr[i];
-        }
-        if (arr[i] > max) {
-            max = arr[i];
-        }
+int printMultipleChars(int x,char letter) {
+    for (int i=0;i<x;i++) {
+        cout<<letter<<" ";
     }
-
-    cout<<min<<" "<<max;
     return 0;
 }
+
 int main() {
     int n;
     cin>>n;
 
-    int arr[n];
+    while (n--) {
+        int x;
+        char letter;
+        cin>>x>>letter;
 
-    for (int i =0;i<n;i++) {
-        cin>>arr[i];
+        printMultipleChars(x,letter);
+        cout<<"\n";
+
     }
-
-    findMinMax(arr,n);
-
 }
