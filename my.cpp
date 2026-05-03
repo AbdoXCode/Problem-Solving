8e069b1935df42d400bd90541e3a4feae0f43b35
Abdelrhman <adodoadodo2025@gmail.com>
2026-05-03 10:13:54 +0300

    A. Print Recursion Assiut university sheet

--- my.cpp
+++ my.cpp
@@ -1,31 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int uniqueNumbers(int n,int arr[]) {
-    int count = 0;
-    for (int i =0;i<n;i++) {
-        for (int j=0;j<n;j++) {
-            if (arr[i] == arr[j]) {
-                if (i <= j) {
-                    count++;
-                    break;
-                }else {
-                    break;
-                }
-            }
-        }
-    }
-    return count;
+void printRecursion(int n) {
+    if (n == 0) return;
+    cout<<"I love Recursion"<<endl;
+    printRecursion(n-1);
 }
+
 int main() {
     int n;
     cin>>n;
 
-    int arr[n];
-
-    for (int i=0;i<n;i++) {
-        cin>>arr[i];
-    }
-
-    cout<<uniqueNumbers(n,arr);
+    printRecursion(n);
 }
