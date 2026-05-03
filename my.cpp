4a2c6be562b302642db75770d8ea421248e39f86
Abdelrhman <adodoadodo2025@gmail.com>
2026-05-03 10:21:42 +0300

    C. Print from N to 1 Assiut university sheet

--- my.cpp
+++ my.cpp
@@ -3,8 +3,12 @@ using namespace std;
 
 void printToN(int n) {
     if (n == 0) return;
+    if (n==1) {
+        cout<<n;
+    }else {
+        cout<<n<<" ";
+    }
     printToN(n-1);
-    cout<<n<<endl;
 }
 
 int main() {
