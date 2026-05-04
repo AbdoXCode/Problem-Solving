01c8b77ff5875f2068b1efd5a8532705c692211c
Abdelrhman <adodoadodo2025@gmail.com>
2026-05-04 07:36:31 +0300

    H. Inverted Pyramid Assiut university sheet

--- my.cpp
+++ my.cpp
@@ -1,12 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int total;
+
 void printPyramid(int n) {
     if (n == 0) return;
-    printPyramid(n-1);
     cout<<string(total - n ,' ');
     cout<<string(2 * n - 1,'*');
     cout<<'\n';
+    printPyramid(n-1);
 }
 
 int main() {
