b199720b79e5e8b6b313570143cba914e490b091
Abdelrhman <adodoadodo2025@gmail.com>
2026-05-04 07:35:28 +0300

    G. Pyramid Assiut university sheet

--- my.cpp
+++ my.cpp
@@ -1,22 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-void printEvenIndices(int n,int index,long long arr[]) {
-    if (index >= n) return;
-
-    printEvenIndices(n,index +2,arr);
-    cout<<arr[index]<<" ";
+int total;
+void printPyramid(int n) {
+    if (n == 0) return;
+    printPyramid(n-1);
+    cout<<string(total - n ,' ');
+    cout<<string(2 * n - 1,'*');
+    cout<<'\n';
 }
 
 int main() {
-    int n;
-    cin>>n;
+    cin>>total;
 
-    long long arr[1001] ={};
-
-    for (int i = 0;i<n;i++) {
-        cin>>arr[i];
-    }
-
-    printEvenIndices(n,0,arr);
+    printPyramid(total);
 }
