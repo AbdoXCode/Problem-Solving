8d976227c1857063c0225d439a6f99394111d832
Abdelrhman <adodoadodo2025@gmail.com>
2026-05-07 16:28:35 +0300

    V - Is it rated? Assiut university sheet

--- my.cpp
+++ my.cpp
@@ -2,16 +2,30 @@
 using namespace std;
 
 int main() {
-    long long a,b,q;
-    cin>>a>>b>>q;
+    int n;
+    cin>>n;
 
-    long long res = q % 3;
+    int arr1[n];
+    int arr2[n];
 
-    if (res == 1) {
-        cout<<a;
-    }else if (res == 2) {
-        cout<<b;
-    }else {
-        cout<<(a^b);
+    int cases;
+
+    for (int i =0;i <n;i++){
+        cin>>arr1[i];
+        cin>>arr2[i];
     }
+
+    for (int i =0;i<n;i++) {
+        if (arr1[i] > arr2[i] || arr1[i] < arr2[i]) {
+            cout<<"rated";
+            return 0;
+        }
+    }
+    for (int i=0;i<n;i++) {
+        if (i > 0 && arr1[i-1] < arr1[i]) {
+            cout<<"unrated";
+            return 0;
+        }
+    }
+    cout<<"maybe";
 }
