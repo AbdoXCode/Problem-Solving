3877032ace69fe95a67dcf20439a7ca691979cd5
Abdelrhman <adodoadodo2025@gmail.com>
2026-05-08 22:07:00 +0300

    S. Is Triangle Assiut university sheet

--- my.cpp
+++ my.cpp
@@ -2,21 +2,13 @@
 using namespace std;
 
 int main() {
-    long long a,b,x,sum = 0;
-    cin>>a>>b>>x;
+    double a,b,c;
+    cin>>a>>b>>c;
 
-    if (a > b) swap(a,b);
-
-    long long first = (a + x - 1) / x;
-    long long last = b/x;
-
-    if (first > last) {
-        cout << 0;
-        return 0;
+    double s = (a + b + c) / 2;
+    if (a + b > c && a + c > b && c + b > a) {
+        cout<<"Valid"<<endl<<sqrt(s*(s-a)*(s-b)*(s-c));
+    }else {
+        cout<<"Invalid";
     }
-
-    long long n = last - first + 1;
-    sum = n*(first + last)/ 2;
-
-    cout<<sum * x;
 }
