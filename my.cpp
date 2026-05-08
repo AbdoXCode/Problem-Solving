fd5cd70a4ed9dae5a62ec21cba34d8c7bc21da9b
Abdelrhman <adodoadodo2025@gmail.com>
2026-05-08 14:36:26 +0300

    I. Divisability Assiut university sheet

--- my.cpp
+++ my.cpp
@@ -2,8 +2,21 @@
 using namespace std;
 
 int main() {
-    long long a,b;
-    cin>>a>>b;
+    long long a,b,x,sum = 0;
+    cin>>a>>b>>x;
 
-    cout<<gcd(a,b)<<" "<<lcm(a,b);
+    if (a > b) swap(a,b);
+
+    long long first = (a + x - 1) / x;
+    long long last = b/x;
+
+    if (first > last) {
+        cout << 0;
+        return 0;
+    }
+
+    long long n = last - first + 1;
+    sum = n*(first + last)/ 2;
+
+    cout<<sum * x;
 }
