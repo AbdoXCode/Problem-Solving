5cbe6a8841d07bfa2dca1808865ffc58e72e4117
Abdelrhman <adodoadodo2025@gmail.com>
2026-05-08 13:14:14 +0300

    H - GCD Assiut university sheet

--- my.cpp
+++ my.cpp
@@ -2,30 +2,8 @@
 using namespace std;
 
 int main() {
-    int n;
-    cin>>n;
+    long long a,b;
+    cin>>a>>b;
 
-    int arr1[n];
-    int arr2[n];
-
-    int cases;
-
-    for (int i =0;i <n;i++){
-        cin>>arr1[i];
-        cin>>arr2[i];
-    }
-
-    for (int i =0;i<n;i++) {
-        if (arr1[i] > arr2[i] || arr1[i] < arr2[i]) {
-            cout<<"rated";
-            return 0;
-        }
-    }
-    for (int i=0;i<n;i++) {
-        if (i > 0 && arr1[i-1] < arr1[i]) {
-            cout<<"unrated";
-            return 0;
-        }
-    }
-    cout<<"maybe";
+    cout<<gcd(a,b)<<" "<<lcm(a,b);
 }
