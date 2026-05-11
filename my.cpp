105656e4e3cecfe8fa9e81802eb64050e519877c
Abdelrhman <adodoadodo2025@gmail.com>
2026-05-11 23:22:04 +0300

    G - Summation of its divisors Assiut university sheet

--- my.cpp
+++ my.cpp
@@ -1,21 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long nCr(int n,int r) {
-    if (r == 0 || n==r) return 1;
-
-    return nCr(n-1,r-1) + nCr(n-1,r);
-}
-long long nPr(int n,int r) {
-    if (r == 1) return n;
-    if (r == 0) return 1;
-
-    return n* nPr((n-1) , r-1);
-}
 int main() {
-    int x,y;
-    cin>>x>>y;
+    long long n,sum = 0;
+    cin>>n;
 
-    cout<<nCr(x,y)<<" "<<nPr(x,y);
-
+    for (int i =1 ;i<=sqrt(n);i++) {
+        if (n%i == 0) {
+            sum += i;
+            if (i != sqrt(n)) {
+                sum+= n/i;
+            }
+        }
+    }
+    cout<<sum;
 }
