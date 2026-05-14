ce8d350bb1df843c8fad958cbe7b0ad75fb3fac4
Abdelrhman <adodoadodo2025@gmail.com>
2026-05-14 14:29:03 +0300

    W. Circles Assiut university sheet

--- my.cpp
+++ my.cpp
@@ -2,23 +2,30 @@
 using namespace std;
 
 int main() {
-    int x;
+    int x1,y1,x2,y2;
+    int x3,y3,x4,y4;
 
-    cin>>x;
+    cin>>x1>>y1>>x2>>y2>>x3>>y3>>x4>>y4;
 
-    for (int i =1 ;i<=x;i++) {
-        if (i%2 == 0) {
-            if (i <= x-1) {
-                cout<<"I love that ";
-            }else {
-                cout<<"I love it ";
-            }
-        }else {
-            if (i <= x-1) {
-                cout<<"I hate that ";
-            }else {
-                cout<<"I hate it ";
-            }
-        }
+    double cx1,cy1,cx2,cy2;
+
+    cx1 = (x1+x2)/2.0;
+    cy1 = (y1+y2)/2.0;
+    cx2 = (x3+x4)/2.0;
+    cy2 = (y3+y4)/2.0;
+
+    double d1 = sqrt(pow(x1-x2,2)+pow(y1-y2,2));
+    double d2 = sqrt(pow(x3-x4,2)+pow(y3-y4,2));
+
+    double r1 = d1/2.0;
+    double r2 = d2/2.0;
+
+    double dis = sqrt(pow(cx1-cx2,2)+pow(cy1-cy2,2));
+
+    if (r1 + r2 < dis) {
+        cout<<"NO";
+    }else {
+        cout<<"YES";
     }
+
 }
