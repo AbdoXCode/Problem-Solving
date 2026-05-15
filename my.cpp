677e2b0c11f555193ad96ea7343decec2b376f6e
Abdelrhman <adodoadodo2025@gmail.com>
2026-05-15 18:18:18 +0300

    1742A - Sum Assiut university sheet

--- my.cpp
+++ my.cpp
@@ -2,31 +2,17 @@
 using namespace std;
 
 int main() {
-    int p;
-    cin>>p;
-
-    set<int> levels;
     int n;
     cin>>n;
-    for (int i =0;i<n;i++) {
-        int x;
-        cin>>x;
-        levels.insert(x);
+
+    while (n--) {
+        int x,y,z;
+        cin>>x>>y>>z;
+
+        if (x + y == z || x + z == y || z + y == x) {
+            cout<<"YES"<<endl;
+        }else {
+            cout<<"NO"<<endl;
+        }
     }
-
-    int q;
-    cin>>q;
-
-    for (int i =0;i<q;i++) {
-        int y;
-        cin>>y;
-        levels.insert(y);
-    }
-
-    if (levels.size() >= p) {
-        cout<<"I become the guy.";
-    }else {
-        cout<<"Oh, my keyboard!";
-    }
-
 }
