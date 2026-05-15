8db2a8532bdc3f10ff709935618fa2c1dabc2588
Abdelrhman <adodoadodo2025@gmail.com>
2026-05-15 17:08:24 +0300

    61A - Ultra-Fast Mathematician Assiut university sheet

--- my.cpp
+++ my.cpp
@@ -2,30 +2,17 @@
 using namespace std;
 
 int main() {
-    int x1,y1,x2,y2;
-    int x3,y3,x4,y4;
+    string x,y,final;
+    cin>>x>>y;
 
-    cin>>x1>>y1>>x2>>y2>>x3>>y3>>x4>>y4;
-
-    double cx1,cy1,cx2,cy2;
-
-    cx1 = (x1+x2)/2.0;
-    cy1 = (y1+y2)/2.0;
-    cx2 = (x3+x4)/2.0;
-    cy2 = (y3+y4)/2.0;
-
-    double d1 = sqrt(pow(x1-x2,2)+pow(y1-y2,2));
-    double d2 = sqrt(pow(x3-x4,2)+pow(y3-y4,2));
-
-    double r1 = d1/2.0;
-    double r2 = d2/2.0;
-
-    double dis = sqrt(pow(cx1-cx2,2)+pow(cy1-cy2,2));
-
-    if (r1 + r2 < dis) {
-        cout<<"NO";
-    }else {
-        cout<<"YES";
+    for (int i =0;i<x.length();i++) {
+        if (x[i] == '1' && y[i]=='0') {
+            final += '1';
+        }else if (x[i] == '0' && y[i]=='1') {
+            final += '1';
+        }else {
+            final+='0';
+        }
     }
-
+    cout<<final;
 }
