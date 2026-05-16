27651cadc0b3c07c49de8a9f18c601426fb2408a
Abdelrhman <adodoadodo2025@gmail.com>
2026-05-16 23:49:03 +0300

    L - String Functions Assiut university sheet

--- my.cpp
+++ my.cpp
@@ -1,18 +1,56 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 int main() {
-    int n;
-    cin>>n;
+    int t,n;
+    cin>>t>>n;
+
+    string x;
+    cin>>x;
 
     while (n--) {
-        int x,y,z;
-        cin>>x>>y>>z;
+        string op;
+        cin>>op;
 
-        if (x + y == z || x + z == y || z + y == x) {
-            cout<<"YES"<<endl;
-        }else {
-            cout<<"NO"<<endl;
+        if (op == "substr") {
+            int l,ll;
+            cin>>l>>ll;
+            for (int i = min(l,ll) - 1;i<max(l,ll);i++) {
+                cout<<x[i];
+            }
+            cout<<endl;
+        }
+        else if (op == "sort") {
+            int l,ll;
+            cin>>l>>ll;
+            sort(x.begin() + min(l,ll) - 1,x.begin() + max(l,ll));
+        }
+        else if (op == "pop_back") {
+            x.pop_back();
+        }
+        else if (op == "back") {
+            cout<<x[x.length()-1]<<endl;
+        }
+        else if (op == "reverse") {
+            int l,ll;
+            cin>>l>>ll;
+            reverse(x.begin() + min(l,ll) - 1,x.begin() + max(l,ll));
+        }
+        else if (op == "front") {
+            cout<<x.front()<<endl;
+        }
+        else if (op == "push_back") {
+            string xx;
+            cin>>xx;
+
+            x += xx;
+        }
+        else if (op == "print") {
+            int pos;
+            cin>>pos;
+
+            cout<<x[pos-1]<<endl;
         }
     }
 }
