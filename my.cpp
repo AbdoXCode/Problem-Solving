6cd2428106e5579d1b6260f2eee0f2d2f2558a2d
Abdelrhman <adodoadodo2025@gmail.com>
2026-05-19 11:38:03 +0300

    X - Rectangle Assiut university sheet

--- my.cpp
+++ my.cpp
@@ -3,19 +3,25 @@
 using namespace std;
 
 int main() {
-    int t;
+    long long x1,x2,x3,x4,y1,y2,y3,y4;
+    cin>>x1>>y1>>x2>>y2>>x3>>y3>>x4>>y4;
+
+    long long t;
     cin>>t;
 
+    long long minX = min(x1,min(x2,min(x3,x4)));
+    long long maxX = max(x1,max(x2,max(x3,x4)));
+    long long minY = min(y1,min(y2,min(y3,y4)));
+    long long maxY = max(y1,max(y2,max(y3,y3)));
+
     while (t--) {
-        long long n,a,b;
-        cin>>n>>a>>b;
+        long long x,y;
+        cin>>x>>y;
 
-        long long groups = n / 3;
-        long long rem = n % 3;
-
-        long long ans = (groups * min(3*a,b) + min(rem * a , b));
-
-        cout<<ans<<endl;
-
+        if ((minX <= x && x <= maxX) && (minY <= y && y <= maxY)) {
+            cout<<"YES"<<endl;
+        }else {
+            cout<<"NO"<<endl;
+        }
     }
 }
