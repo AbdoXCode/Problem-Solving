d5fa19d545ddf378a9e473dc8646b921c2c12694
Abdelrhman <adodoadodo2025@gmail.com>
2026-05-20 15:16:19 +0300

    V. Two Lines Assiut university sheet

--- my.cpp
+++ my.cpp
@@ -1,11 +1,19 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <thread>
+#include <chrono>
 
 using namespace std;
 
 int main() {
-    string s;
+    int x1,x2,x3,x4,y1,y2,y3,y4;
+    cin>>x1>>y1>>x2>>y2>>x3>>y3>>x4>>y4;
 
-    while (getline(cin,s)) {
-        cout<<"NO"<<endl;
+    long long slope1 = (x2-x1) * (y4-y3);
+    long long slope2 = (y2-y1) * (x4-x3);
+
+    if (slope1 == slope2) {
+        cout<<"YES";
+    }else {
+        cout<<"NO";
     }
 }
