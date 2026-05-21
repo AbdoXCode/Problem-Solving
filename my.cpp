3a73eecdf802e85dc8c0081988171fea75336ed4
Abdelrhman <adodoadodo2025@gmail.com>
2026-05-21 15:41:27 +0300

    1873A - Short Sort

--- my.cpp
+++ my.cpp
@@ -5,31 +5,22 @@ int main() {
     int t;
     cin>>t;
 
+    string target = "abc";
     while (t--) {
-        int n;
-        cin>>n;
+        string s;
+        cin>>s;
 
-        int arr[n];
-
-        for (int i =0;i<n;i++) {
-            cin>>arr[i];
-        }
-
-        int normal;
-
-        if (arr[0] == arr[1])
-            normal = arr[0];
-        else if (arr[0] == arr[2])
-            normal = arr[0];
-        else
-            normal = arr[1];
-
-        for (int i = 0; i < n; i++) {
-            if (normal != arr[i]) {
-                cout<<i + 1<<endl;
-                break;
+        int num = 0;
+        for (int i =0;i<s.length();i++) {
+            if (s[i] != target[i]) {
+                num +=1;
             }
         }
 
+        if (num <= 2) {
+            cout<<"YES"<<endl;
+        }else {
+            cout<<"NO"<<endl;
+        }
     }
 }
