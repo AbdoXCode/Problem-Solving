a456389ed772ec9f8559b61d14865595246bd566
Abdelrhman <adodoadodo2025@gmail.com>
2026-05-27 00:45:59 +0300

    K - Shift Right Assiut university sheet

--- my.cpp
+++ my.cpp
@@ -1,20 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int t;
-    cin>>t;
-
-    while (t--) {
-        int x;
-        cin>>x;
-
-        int animals = x / 4;
-
-        if (x % 4 != 0) {
-            animals++;
+auto shiftArray(int arr[],int n,int x) {
+    while (x--) {
+        int last = arr[n-1];
+        for (int i =n-1;i>=0;i--) {
+            if (i == 0) {
+                arr[0] = last;
+            }else {
+                arr[i] = arr[i-1];
+            }
         }
+    }
+}
+int main(){
+    int n,x;
+    cin>>n>>x;
 
-        cout<<animals<<endl;
+    int arr[n];
+
+    for (int i=0;i<n;i++) {
+        cin>>arr[i];
+    }
+
+    shiftArray(arr,n,x);
+
+    for (int i=0;i<n;i++) {
+        cout<<arr[i]<<" ";
     }
 }
