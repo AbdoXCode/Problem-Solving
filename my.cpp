fa3ac27a058c859b915c52908b623fa9837e2c02
Abdelrhman <adodoadodo2025@gmail.com>
2026-05-28 10:38:56 +0300

    O - Five in One Assiut university sheet

--- my.cpp
+++ my.cpp
@@ -1,28 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void shiftZeros(int arr[],int n) {
-    vector<int> arrShifted;
-    int zeros = 0;
+bool isPrime(int n) {
+    if (n<=1) return false;
 
-    for (int i =0;i<n;i++) {
-        if (arr[i] != 0) {
-            arrShifted.push_back(arr[i]);
-        }else {
-            zeros ++;
+    for (int i =2;i<n;i++) {
+        if (n % i == 0) {
+            return false;
         }
     }
-
-    while (zeros--) {
-        arrShifted.push_back(0);
+    return true;
+}
+int isPalindrome(int n) {
+    if (n <0) return false;
+    int newNum = 0;
+    int originalNum = n;
+    while (n>0) {
+        int lastNum = n % 10;
+        newNum = (newNum * 10) + lastNum;
+        n /=10;
     }
-    for (int i =0;i<n;i++) {
-        cout<<arrShifted[i]<<" ";
+    return newNum == originalNum;
+}
+int maxDivisors(int n) {
+    int maxDiv = 0;
+    for (int i = 1;i<n;i++) {
+        if (n % i ==0) {
+            maxDiv ++;
+        }
     }
-
+    return maxDiv;
 }
 int main(){
-    int n;
+    int n,maximum = 0,minimum = 99999,primeNumbers = 0,palindromeNumbers = 0;
     cin>>n;
 
     int arr[n];
@@ -31,7 +41,35 @@ int main(){
         cin>>arr[i];
     }
 
-    shiftZeros(arr,n);
+    for (int i =0;i<n;i++) {
+        maximum = max(arr[i],maximum);
+        minimum = min(arr[i],minimum);
+        if (isPrime(arr[i])) {
+            primeNumbers ++;
+        }
+        if (isPalindrome(arr[i])) {
+            palindromeNumbers++;
+        }
+    }
 
+    int maxDivisor = 0;
+    int bestNum = 0;
 
+    for (int x:arr) {
+        int div = maxDivisors(x);
+
+        if (div > maxDivisor) {
+            maxDivisor = div;
+            bestNum = x;
+        }else if (div == maxDivisor) {
+            if (x > bestNum) {
+                bestNum = x;
+            }
+        }
+    }
+    cout<<"The maximum number : "<<maximum<<endl;
+    cout<<"The minimum number : "<<minimum<<endl;
+    cout<<"The number of prime numbers : "<<primeNumbers<<endl;
+    cout<<"The number of palindrome numbers : "<<palindromeNumbers<<endl;
+    cout<<"The number that has the maximum number of divisors : "<<bestNum;
 }
