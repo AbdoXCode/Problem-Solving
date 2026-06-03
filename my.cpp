1b1b83e51fe54abddee022330529d332c6ec0bbb
Abdelrhman <adodoadodo2025@gmail.com>
2026-06-03 14:11:25 +0300

    2218A - The 67th Integer Problem

--- my.cpp
+++ my.cpp
@@ -2,18 +2,19 @@
 using namespace std;
 
 int main(){
-    int t,sum = 0,maxSum = 0;
+    int t;
     cin>>t;
 
     while (t--) {
-        int x,y;
-        cin>>x>>y;
+        int x;
+        cin>>x;
 
-        sum-=x;
-        sum+=y;
-        maxSum = max(sum,maxSum);
+        if (x < 0) {
+            cout<<x<<endl;
+        }else if (x >= 67){
+            cout<<67<<endl;
+        }else {
+            cout<<++x<<endl;
+        }
     }
-
-    cout<<maxSum<<endl;
-
 }
