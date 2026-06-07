0792702961b97b9cb37eea4e65a7d4c77bb82d3a
Abdelrhman <adodoadodo2025@gmail.com>
2026-06-07 18:25:52 +0300

    1030A - In Search of an Easy Problem

--- my.cpp
+++ my.cpp
@@ -2,26 +2,17 @@
 using namespace std;
 
 int main(){
-    int n,k,sum =0;
-    cin>>n>>k;
+    int n;
+    cin>>n;
 
     int arr[n];
+
     for (int i =0;i<n;i++) {
         cin>>arr[i];
-        sum +=arr[i];
+        if (arr[i] == 1) {
+            cout<<"HARD";
+            return 0;
+        }
     }
-
-    int i = 0;
-    int steps = 0;
-
-    while (arr[k -1] <= arr[i] && n > i && arr[i] != 0) {
-        i++;
-        steps++;
-    }
-
-    if (sum >0) {
-        cout<<steps;
-    }else {
-        cout<<0;
-    }
+    cout<<"EASY";
 }
