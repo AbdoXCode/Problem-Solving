4702707b30b22c90e461f7ac1968d62100c786fd
Abdelrhman <adodoadodo2025@gmail.com>
2026-06-08 10:23:03 +0300

    1669A - Division?

--- my.cpp
+++ my.cpp
@@ -2,17 +2,21 @@
 using namespace std;
 
 int main(){
-    int n;
-    cin>>n;
+    int t;
+    cin>>t;
 
-    int arr[n];
+    while (t--) {
+        int n;
+        cin>>n;
 
-    for (int i =0;i<n;i++) {
-        cin>>arr[i];
-        if (arr[i] == 1) {
-            cout<<"HARD";
-            return 0;
+        if (n <= 1399) {
+            cout<<"Division 4"<<endl;
+        }else if (n >=1400 && n<=1599) {
+            cout<<"Division 3"<<endl;
+        }else if (n >= 1600 && n<= 1899) {
+            cout<<"Division 2"<<endl;
+        }else {
+            cout<<"Division 1"<<endl;
         }
     }
-    cout<<"EASY";
 }
