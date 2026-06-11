5f6274b2bba435f064e063530488e958fccbd0e8
Abdelrhman <adodoadodo2025@gmail.com>
2026-06-11 13:44:41 +0300

    1950A - Stair, Peak, or Neither?

--- my.cpp
+++ my.cpp
@@ -6,23 +6,15 @@ int main(){
     cin>>t;
 
     while (t--) {
-        string x;
-        cin>>x;
+        int a,b,c;
+        cin>>a>>b>>c;
 
-        string a1 = x.substr(0,3);
-        string a2 = x.substr(3);
-
-        int sum1 = 0;
-        int sum2 = 0;
-
-        for (char c:a1) {
-            sum1 += (c -'0');
+        if (a<b && b<c) {
+            cout<<"STAIR"<<endl;
+        }else if (a<b && b>c) {
+            cout<<"PEAK"<<endl;
+        }else {
+            cout<<"NONE"<<endl;
         }
-        for (char c:a2) {
-           sum2 += (c-'0');
-        }
-
-        if (sum1 == sum2) cout<<"YES"<<endl;
-        else cout<<"NO"<<endl;
     }
 }
