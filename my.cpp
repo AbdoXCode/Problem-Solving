adc58beda45f82b3c8e0f2058b0680fac868557c
Abdelrhman <adodoadodo2025@gmail.com>
2026-06-19 12:35:14 +0300

    E - Alternating Array Assiut university sheet

--- my.cpp
+++ my.cpp
@@ -2,17 +2,25 @@
 using namespace std;
 
 int main(){
-   int t;
-   cin>>t;
+   int n;
+   cin>>n;
 
-   while (t--) {
-      int a,b,c;
-      cin>>a>>b>>c;
+   int op1 = 0;
+   for (int i =0;i<n;i++) {
+      int a;
+      cin>>a;
 
-      int arr[3] ={a,b,c};
-
-      sort(arr,arr+3);
-
-      cout<<arr[1]<<endl;
+      if (i % 2 == 0) {
+         if (a<0) {
+            op1++;
+         }
+      }else {
+         if (a>0) {
+            op1++;
+         }
+      }
    }
+   int op2 = n - op1;
+   cout<<min(op1,op2)<<endl;
+
 }
